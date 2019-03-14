e6bd83e041f5dc976d2434ff6770dabaefdc94a1
real-neo <questionyugood@gmail.com>
2019-03-14 15:53:56 +0000

    Use glob find files recursively.

--- extract.py
+++ extract.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import glob
 import os
 from typing import List, Set
 
@@ -6,7 +7,7 @@ from typing import List, Set
 def convert_bytes(size: int) -> str:
     for x in ['B', 'KiB', 'MiB', 'GiB', 'TiB']:
         if size < 1024.0:
-            return "%3.1f %s" % (size, x)
+            return '%3.1f %s' % (size, x)
         size /= 1024.0
 
 
@@ -36,20 +37,31 @@ def suffix_list(directory: str) -> Set:
 
 
 def dir_detail_java(directory: str):
-    for f in file_list(directory):
-        if f.endswith('.java'):
-            print(f, file_size(f), file_line(f))
+    _list = glob.glob(os.path.join(directory, '**/*.java'), recursive=True)
+    print('Java file numbers:', len(_list))
+    for f in _list:
+        print(f, file_size(f), file_line(f))
 
 
-def dir_detail(directory: str):
-    for f in file_list(directory):
+def dir_detail_text(directory: str):
+    _list = file_list(directory)
+    print('Text file numbers:', len(_list))
+    for f in _list:
         if ('.git' not in f) and ('.idea' not in f) and ('.DS_Store' not in f) and (not f.endswith('.zip')) and \
                 (not f.endswith('.gif')) and (not f.endswith('.png')) and (not f.endswith('.jar')):
             print(f, file_size(f), file_line(f))
 
 
-if __name__ == "__main__":
-    gson_dir = "/Users/neo/IdeaProjects/gson"
-    # print(suffix_list(directory))
-    dir_detail(gson_dir)
+def list_all_file(directory: str):
+    _list = glob.glob(os.path.join(directory, '**/*'), recursive=True)
+    for f in _list:
+        if os.path.isfile(f):
+            print(f)
+
+
+if __name__ == '__main__':
+    gson_dir = '/Users/neo/IdeaProjects/gson'
+    # print(suffix_list(gson_dir))
+    # list_all_file(gson_dir)
+    dir_detail_text(gson_dir)
     # dir_detail_java(gson_dir)
