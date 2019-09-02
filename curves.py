a29a3783d65b08507396634f0313220dfd9c249c
real-neo <questionyugood@gmail.com>
2019-09-02 17:31:26 +0100

    Remove some useless code.

--- curves.py
+++ curves.py
@@ -35,34 +35,35 @@ def draw_loc_curve(file):
     draw_curve(code.LOC, 'LOC', 'Code_Index', 'LOC', color='blue')
 
 
-def draw_pdf(dataset, x_label):
-    fig, ax = plt.subplots()
+# def draw_pdf(dataset, x_label):
+#     fig, ax = plt.subplots()
+#
+#     figPDF = powerlaw.plot_pdf(dataset, ax, color='b')
+#     powerlaw.plot_pdf(dataset, linear_bins=True, color='r', ax=figPDF)
+#
+#     figPDF.set_title(x_label + ' PDF')
+#     figPDF.set_xlabel(x_label)
+#     figPDF.set_ylabel("p(X)")
+#     figname = 'FigPDF'
+#     fig.show()
 
-    figPDF = powerlaw.plot_pdf(dataset, ax, color='b')
-    powerlaw.plot_pdf(dataset, linear_bins=True, color='r', ax=figPDF)
 
-    figPDF.set_title(x_label + ' PDF')
-    figPDF.set_xlabel(x_label)
-    figPDF.set_ylabel("p(X)")
-    figname = 'FigPDF'
-    fig.show()
-
-
-def draw_ccdf(dataset, x_label):
+def draw_pdf_ccdf(dataset, x_label):
     fit = powerlaw.Fit(dataset, discrete=True)
 
     fig, ax = plt.subplots()
 
-    figCCDF = fit.plot_pdf(ax, color='b', linewidth=2)
-    fit.power_law.plot_pdf(color='b', linestyle='--', ax=figCCDF)
-    fit.plot_ccdf(color='r', linewidth=2, ax=figCCDF)
-    fit.power_law.plot_ccdf(color='r', linestyle='--', ax=figCCDF)
+    figCCDF = fit.plot_pdf(ax, color='b', linewidth=2, label='PDF')
+    fit.power_law.plot_pdf(color='b', linestyle='--', ax=figCCDF, label='power law fit PDF')
+    fit.plot_ccdf(color='r', linewidth=2, ax=figCCDF, label='CCDF')
+    fit.power_law.plot_ccdf(color='r', linestyle='--', ax=figCCDF, label='power law fit CCDF')
 
     figCCDF.set_title(x_label + ' CCDF')
     figCCDF.set_xlabel(x_label)
     figCCDF.set_ylabel(u"p(X),  p(X≥x)")
 
     figname = 'FigCCDF'
+    fig.legend()
     fig.show()
 
 
@@ -70,16 +71,16 @@ def draw_population(file):
     population = pd.read_csv(file)
     population.sort_values(by='Population', ascending=False, inplace=True)
     population = population.reset_index()
-    draw_pdf(population.Population, 'Population')
-    draw_ccdf(population.Population, 'Population')
+    # draw_pdf(population.Population, 'Population')
+    draw_pdf_ccdf(population.Population, 'Population')
 
 
 def draw_loc(file):
     code = pd.read_csv(file)
     code.sort_values(by='LOC', ascending=False, inplace=True)
     code = code.reset_index()
-    draw_pdf(code.LOC, 'LOC')
-    draw_ccdf(code.LOC, 'LOC')
+    # draw_pdf(code.LOC, 'LOC')
+    draw_pdf_ccdf(code.LOC, 'LOC')
 
 
 if __name__ == '__main__':
